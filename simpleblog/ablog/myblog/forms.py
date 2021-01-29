e32889303a9319e43a920df829092d0b14bd2e82
linika <trialxxx@gmail.com>
2021-01-29 17:35:57 +0530

    simpleblog commit

---
+++ simpleblog/ablog/myblog/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from .models import Post, Category, Comment
+
+#choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment')]
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippets', 'header_image')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'elder', 'type':'hidden'}),
+        #    'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippets': forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields = ('title', 'title_tag', 'body', 'snippets')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is Title Placeholder'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'snippets': forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields=('name', 'body')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
